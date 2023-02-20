4e8a97ae77f1bdbadb1467261f5f38cfb29f3d31
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2023-02-20 16:52:52 +0100

    assignment 2 init

--- homeworks/assignment02_three_headed_network/network.py
+++ homeworks/assignment02_three_headed_network/network.py
@@ -13,30 +13,42 @@ class ThreeInputsNet(nn.Module):
     def __init__(self, n_tokens, n_cat_features, concat_number_of_features, hid_size=64):
         super(ThreeInputsNet, self).__init__()
         self.title_emb = nn.Embedding(n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>        
+        self.title = nn.Sequential(
+            nn.Conv1d(hid_size, hid_size * 2, 3),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool1d(1),
+        )   
         
         self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>
+        self.full = nn.Sequential(
+            nn.Conv1d(hid_size, hid_size * 2, 3),
+            nn.ReLU(),
+            nn.AdaptiveMaxPool1d(1),
+        )
         
-        self.category_out = # <YOUR CODE HERE>
+        self.category_out = nn.Sequential(
+            nn.Linear(n_cat_features, hid_size),
+            nn.ReLU(),
+        )
 
 
         # Example for the final layers (after the concatenation)
-        self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2)
-        self.final_dense = nn.Linear(in_features=hid_size*2, out_features=1)
-
+        self.clf = nn.Sequential(
+            nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2),
+            nn.ReLU(),
+            nn.Linear(in_features=hid_size*2, out_features=1),
+        )
         
 
     def forward(self, whole_input):
         input1, input2, input3 = whole_input
         title_beg = self.title_emb(input1).permute((0, 2, 1))
-        title = # <YOUR CODE HERE>
+        title = self.title(title_beg)
         
         full_beg = self.full_emb(input2).permute((0, 2, 1))
-        full = # <YOUR CODE HERE>        
+        full = self.full(full_beg)      
         
-        category = # <YOUR CODE HERE>        
-        
+        category = self.category_out(input3)     
         concatenated = torch.cat(
             [
             title.view(title.size(0), -1),
@@ -44,7 +56,6 @@ class ThreeInputsNet(nn.Module):
             category.view(category.size(0), -1)
             ],
             dim=1)
-        
-        out = # <YOUR CODE HERE>
+        out = self.clf(concatenated)
         
         return out
